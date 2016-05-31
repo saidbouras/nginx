bf518d33aa63de9912ad40ec3d70299e35c0de6e
Said <said.bouras@zenika.com>
2016-05-31 10:25:36 +0200

    [#] check if nginx configuration file already exist

--- root/etc/cont-init.d/01-init-nginx.sh
+++ root/etc/cont-init.d/01-init-nginx.sh
@@ -6,6 +6,11 @@ if [ ! -d /etc/nginx/sites-available ]; then
     mkdir /etc/nginx/sites-available
     rm -rf /etc/nginx/conf.d/*
     mv /etc/nginx/default /etc/nginx/sites-available/default
+
+    # check if there is not a previous version of default
+    if [ ! -e /etc/nginx/sites-enabled/default ]; then
+        rm -rf /etc/nginx/sites-enabled/default
+    fi
     ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 fi
 
